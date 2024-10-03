11ed67fbddc4c922f39f27011a9ddc51a16b8437
moses197 <leomoszeko@gmail.com>
2024-10-03 16:25:03 +0100

    step 2 and 3 done made some changes

--- lib/bittorrent.ex
+++ lib/bittorrent.ex
@@ -19,16 +19,17 @@ defmodule Bittorrent.CLI do
 end
 
 defmodule Bencode do
-    def decode(encoded_value) when is_binary(encoded_value) do
-        binary_data = :binary.bin_to_list(encoded_value)
-        case Enum.find_index(binary_data, fn char -> char == 58 end) do
-          nil ->
-            IO.puts("The ':' character is not found in the binary")
-          index ->
-            rest = Enum.slice(binary_data, index+1..-1)
-            List.to_string(rest)
-        end
-      end
+  def decode(encoded_value) when is_binary(encoded_value) do
+    binary_data = :binary.bin_to_list(encoded_value)
+    case Enum.find_index(binary_data, &(&1 == ?:)) do
+      nil ->
+        IO.puts("The ':' character is not found in the binary")
+      index ->
+        length_data = length(binary_data)
+        rest = Enum.slice(binary_data, index+1..(length_data - 1))
+        {:ok, List.to_string(rest)}
+    end
+  end
 
     def decode(_), do: "Invalid encoded value: not binary"
 end
